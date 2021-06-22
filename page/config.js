5bc1cde2286dc3ed5a21e3710a177cc459d63889
Jakub Tomkiewicz <kubatomki@gmail.com>
2021-06-22 20:14:30 +0200

    dzialajacy plik konfiguracyjny

---
+++ page/config.js
@@ -0,0 +1,16 @@
+//TO JEST PLIK KONFIGURACYJNY 
+
+//KOLORY
+var zielony = 10;
+var pomaranczowy = 30;
+var czerwony = 50;
+
+//MNOZNIK
+var mnoznik = 1;
+
+//MINIMUM MAKSIMUM
+var minimum = -50;
+var maksimum = 50;
+
+//ZNACZNIKI
+var znaczniki = [-50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50];
